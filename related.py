ff5d0183fedfe5934b160af2495d6d620aa968d0
sabrinaliuu <106926085+sabrinaliuu@users.noreply.github.com>
2025-07-23 19:50:12 +0900

    Update related.py

--- related.py
+++ related.py
@@ -5,8 +5,9 @@ rows['related'] = 0
 keyword = ["能登", "復興"]
     
 for i in range(len(rows)):
-    label = any(word in rows['sentence'].iloc[i] for word in keyword)
+    label = any(word in rows.loc[i, 'sentence'] for word in keyword)
     flag = 1 if label == True else 0
-    rows['related'].iloc[i] = flag
+    rows.loc[i,'related'] = flag
 
-head(rows)
+print(rows.head())
+
